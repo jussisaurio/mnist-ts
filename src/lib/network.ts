fc79ce41a342ca48e18f974b949a0c45ec0e32dc
Jussi Saurio <jussi.saurio@gmail.com>
2023-02-08 12:40:55 +0200

    Rewrite a bunch of comments to the backprop algorithm

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -102,27 +102,78 @@ export function multiLayerBackPropagate(
   const [__, outputLayerA] =
     outputsAndActivations[outputsAndActivations.length - 1];
 
+  // We want to calculate the derivative of the cost function with respect to the weights and biases,
+  // I.e. how much the cost function changes when we change the weights and biases.
+  // Using these derivatives, we can update the weights and biases to reduce the cost function, i.e. reduce how much our network is wrong.
   const dW: any[] = [];
   const dB: any[] = [];
 
-  const dZOutput = mSub(outputLayerA, expected);
+  // To update the weights, we want to calculate the derivative of the cost function with respect to the weights.
+  // Using the chain rule:
+  // dLoss/dW_Output = dLoss/dZ_Output * dZ_Output/dW_Output
+  //
+  // About calculating dLoss/dZ_Output:
+  // We are implicitly using Cross-Entropy loss here.
+  // When using softmax, the derivative of the C-E loss function with respect to the output layer's weighted sum, z,
+  // is obtained from a complicated calculation (http://www.adeveloperdiary.com/data-science/deep-learning/neural-network-with-softmax-in-python/),
+  // but in the end it simplifies to just the difference between the output and the expected output. Trust me bro, it's true.
+  // For this reason we don't have to calculate anything except:
+  const dL_dZOutput = mSub(outputLayerA, expected);
 
   const [___, lastHiddenLayerA] = hiddenLayers[hiddenLayers.length - 1];
 
-  dW.unshift(mSD(mMul(dZOutput, T(lastHiddenLayerA)), batchSize));
-  dB.unshift(mSD(dZOutput, batchSize));
+  // For dZ_Output/dW_Output, we can expand Z_Output to = W_Output * A_Prev + B_Output
+  // So: dZ_Output/dW_Output = A_Prev
+  // So: dLoss/dW_Output = dLoss/dZ_Output * A_Prev
+  dW.unshift(mSD(mMul(dL_dZOutput, T(lastHiddenLayerA)), batchSize)); // divide by batch size to get the average
 
-  let dZPrev = dZOutput;
+  // dLoss/dB_Output = dLoss/dZ_Output * dZ_Output/dB_Output
+  // For dZ_Output/dB_Output, we can expand Z_Output to = W_Output * A_Prev + B_Output
+  // So: dZ_Output/dB_Output = 1
+  // So: dLoss/dB_Output = dLoss/dZ_Output * 1
+  dB.unshift(mSD(dL_dZOutput, batchSize)); // divide by batch size to get the average
+
+  // By 'next', we mean the layer that is closer to the output layer. We are going backwards through the layers.
+  let dZNext = dL_dZOutput;
+
   for (let i = hiddenLayers.length - 1; i >= 0; i--) {
-    const [hiddenZ] = hiddenLayers[i];
-    const nextWeights = T(weights[i + 1]);
-    const dZ = mMul(nextWeights, dZPrev).map((row, j, self) => {
-      return row.map((v, k) => v * reluDerivative(hiddenZ[j][k]));
+    // For the other layers, we can use the same process as above, but we need to use the derivative of the activation function.
+    // Again using the chain rule:
+    // dLoss/dZ_Hidden = dLoss/dZ_Next * dZ_Next/dA_Hidden * dA_Hidden/dZ_Hidden
+
+    // We already have dLoss/dZNext in the variable: dZNext.
+    dZNext = dZNext;
+
+    // For dZ_Next/dA_Hidden, we can expand Z_Next to = W_Next * A_Hidden + B_Next
+    // So: dZ_Next/dA_Hidden = W_Next
+    const W_Next = weights[i + 1];
+
+    // For dA_Hidden/dZ_Hidden, we can use the derivative of the activation function.
+    // Since A_Hidden = relu(Z_Hidden):
+    // dA_Hidden/dZ_Hidden = relu'(Z_Hidden) * 1 // the 1 comes from the chain rule f'(g(x)) * g'(x)
+    // So: dLoss/dZ_Hidden = dZNext * W_Next * relu'(Z_Hidden)
+    const [Z_Hidden] = hiddenLayers[i];
+
+    const dZ_Hidden = mMul(T(W_Next), dZNext).map((row, j) => {
+      return row.map((v, k) => v * reluDerivative(Z_Hidden[j][k]));
     });
-    const prevA = i === 0 ? inputLayerA : hiddenLayers[i - 1][1];
-    dW.unshift(mSD(mMul(dZ, T(prevA)), batchSize));
-    dB.unshift(mSD(dZ, batchSize));
-    dZPrev = dZ;
+
+    // dLoss/dW_Hidden = dLoss/dZHidden * dZ_Hidden/dW_Hidden
+    // For dZ_Hidden/dW_Hidden, we can expand Z_Hidden to = W_Hidden * A_Prev + B_Hidden
+    // So: dZ_Hidden/dW_Hidden = A_Prev
+
+    // So: dLoss/dW_Hidden = dLoss/dZ_Hidden * A_Prev
+    const A_Prev = i === 0 ? inputLayerA : hiddenLayers[i - 1][1];
+    dW.unshift(mSD(mMul(dZ_Hidden, T(A_Prev)), batchSize)); // divide by batch size to get the average
+
+    // dLoss/dB_Hidden = dLoss/dZ_Next * dZ_Next/dA_Hidden * dA_Hidden/dZ_Hidden * dZ_Hidden/dB_Hidden
+    // For dZ_Hidden/dB_Hidden, we can expand Z_Hidden to = W_Hidden * A_Prev + B_Hidden
+    // So: dZ_Hidden/dB_Hidden = 1
+
+    dB.unshift(mSD(dZ_Hidden, batchSize)); // divide by batch size to get the average
+
+    // For the next iteration, we want to calculate dLoss/dZ_Prev, so we set dZNext to dZ_Hidden.
+    dZNext = dZ_Hidden;
   }
 
   return {
